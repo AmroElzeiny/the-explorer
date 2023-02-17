484ac7523283bf3ea717195d3715a4c521f9ea5a
AmroElzeiny <amroelzene@gmail.com>
2023-02-17 08:29:04 +0200

    deploy my react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './header/header.jsx';
+import Section1 from './section1/section1.jsx';
+import Section2 from './section2/section2.jsx';
+import Section3 from './section3/section3.jsx';
+import Section4 from './section4/section4.jsx';
+import Section5 from './section5/section5.jsx';
+import Footer from './footer/footer.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Section1/>
+      <Section2/>
+      <Section3/>
+      <Section4/>
+      <Section5/>
+      <Footer/>
     </div>
   );
 }
